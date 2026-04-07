a82d19ab87cdaeed3b06a38df32e45b780917086
nodiraerkinova <nodiraerkinova2010@gmail.com>
2026-04-07 18:00:51 +0500

    coment

--- return-func.py
+++ return-func.py
@@ -1,33 +1,58 @@
-# def is_even(number):
-#     if number % 2 == 0:
-#         return "Juft"
-#     else:
-#         return "False"
+# # def is_even(number):
+# #     if number % 2 == 0:
+# #         return "Juft"
+# #     else:
+# #         return "False"
     
+# # print(is_even(4)) # juft
+# # result = is_even(7)
+# # print(result)
+
+# # Ternary operator yordamida yuqoridagi funksiyani qisqartirish mumkin:
+# # syntax: value_if_true if condition else value_if_fal
+
+# def is_even(number):
+#     return "juft" if number % 2 == 0 else "toq"
+
 # print(is_even(4)) # juft
-# result = is_even(7)
-# print(result)
+# print(is_even(7)) # toq
 
-# Ternary operator yordamida yuqoridagi funksiyani qisqartirish mumkin:
-# syntax: value_if_true if condition else value_if_fal
+# volwes = ["a", "o", "i", "u", "e"]
+# def count_volwes(text):
+#     count = 0
+#     for char in text:
+#         if char in volwes:
+#             count += 1 
+#     return count
 
-def is_even(number):
-    return "juft" if number % 2 == 0 else "toq"
+# print(count_volwes("javascript"))
+# print(count_volwes("python"))
 
-print(is_even(4)) # juft
-print(is_even(7)) # toq
+# # string bo'yicha for loop ishlatish
+# # text = "Hello world"
+# # for char in text:
+# #     print(char)
 
-volwes = ["a", "o", "i", "u", "e"]
-def count_volwes(text):
-    count = 0
-    for char in text:
-        if char == volwes[0] or char == volwes[1] or char == volwes[2] or char == volwes[3] or char == volwes[4]:
-            count += 1
-    return count
-print(count_volwes("javaskrib"))
-print(count_volwes("python"))
+# def toliq_ism_yasa(ism, familiya, otasining_ismi=''):
+#     """Toliq isma qaytaruvchi funksiya"""
+#     if otasining_ismi: # otasining_ismi mavjudligini tekshiramiz
+#         toliq_ism = f"{ism} {otasining_ismi} {familiya}"
+#     else:
+#         toliq_ism = f"{ism} {familiya}"
+#     return toliq_ism.title() 
 
-# string bo'yicha for loop ishlatish
-# text = "Hello world"
-# for char in text:
-#     print(char)
+# talaba1 = toliq_ism_yasa('olim','hakimov') #otasining_ismi kiritilmadi
+# talaba2 = toliq_ism_yasa('hakim','olimov','abrorovich')
+# print(f"Darsga kelmagan talabalar: {talaba1} va {talaba2}")
+
+# def avto_info(kompaniya, model, rangi, korobka, yili, narhi=None):
+#     avto = {'kompaniya':kompaniya,
+#             'model':model,
+#             'rang':rangi,
+#             'korobka':korobka,
+#             'yil':yili,
+#             'narh':narhi}
+#     return avto
+
+# avto1 = avto_info('GM', 'Malibu', 'Qora', 'Avtomat', 2020)
+# print(avto1)        
